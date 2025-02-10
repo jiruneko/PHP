d3165cd261057663c77f8d74c04e136716bb6fec
jiruneko <oohikaru310@gmail.com>
2025-02-11 07:07:41 +0900

    second commit

--- app/array/array.php
+++ app/array/array.php
@@ -1,9 +1,9 @@
 <pre>
 <?php
-// $friends = array("はるき", "かおる", "ひでと");
-// $friends[5] = "まさとし";
-// $friends[2] = "よしき";
-// var_dump($friends);
+$friends = array("はるき", "かおる", "ひでと");
+$friends[5] = "まさとし";
+$friends[2] = "よしき";
+var_dump($friends);
 
 $class1 = array("はるき", "かおる", "ひでと");
 $class2 = array("ゆきこ", "ゆか", "まなみ");
--- app/array/make_associative.php
+++ app/array/make_associative.php
@@ -1,29 +1,29 @@
 <pre>
 <?php
-// $result1 = array(
-//     "Japanese" => 80,
-//     "math" => 75,
-//     "science" => 90
-// );
-// var_dump($result1);
+$result1 = array(
+    "Japanese" => 80,
+    "math" => 75,
+    "science" => 90
+);
+var_dump($result1);
 
-// $result1["math"] = 85;
-// var_dump($result1);
+$result1["math"] = 85;
+var_dump($result1);
 
-// $result2 = [
-//     "history" => 85,
-//     "English" => 80
-// ];
-// $result2["music"] = 90;
-// var_dump($result2);
+$result2 = [
+    "history" => 85,
+    "English" => 80
+];
+$result2["music"] = 90;
+var_dump($result2);
 
-// $result = array("math" => 90, "English" => 80);
+$result = array("math" => 90, "English" => 80);
 
-// $friends = array("Haruki" => $result);
-// var_dump($friends);
+$friends = array("Haruki" => $result);
+var_dump($friends);
 
-// $friends["Kaoru"] = array("math" => 95, "English" => 85);
-// var_dump($friends);
+$friends["Kaoru"] = array("math" => 95, "English" => 85);
+var_dump($friends);
 
 $friends = array("はるき", "かおる", "ひでと");
 for ($i = 0; $i < 3; $i++) {

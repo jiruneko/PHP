385289172a892b4013b129e637f1a1a58823fd78
jiruneko <oohikaru310@gmail.com>
2025-02-23 08:00:19 +0900

    Link

---
+++ academy/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $a = 'hello';
+    echo 10 * 4 + 5 - 2
+    ?>
+</body>
+
+</html>
--- basic/function.php
+++ basic/function.php
@@ -1,32 +1,61 @@
 <?php
-function strcat($left, $right)
+// function strcat($left, $right)
+// {
+//     return $left . $right;
+// }
+// $first = "This is a";
+// $second = " complete sentence!";
+
+// echo strcat($first, $second) . PHP_EOL;
+
+// $a = 3;
+
+// function foo()
+// {
+//     global $a;
+
+//     $a += 2;
+// }
+
+// foo();
+// echo $a;
+
+// function sayIt()
+// {
+//     $ws = <<< "StufftoSay"
+//     The quick brown fox
+//     Jumps over the lazy dog.
+//     StufftoSay;
+//     return $ws;
+// }
+
+// echo sayIt();
+
+function get_price($price)
 {
-    return $left . $right;
+    $price = $price * 1.1;
+    return round($price);
 }
-$first = "This is a";
-$second = " complete sentence!";
+echo get_price(300);
 
-echo strcat($first, $second) . PHP_EOL;
-
-$a = 3;
-
-function foo()
+function default_demo($name = '太郎')
 {
-    global $a;
-
-    $a += 2;
+    echo "私の名前は" . $name . "です<br>";
 }
+default_demo("花子");
+default_demo();
 
-foo();
-echo $a;
-
-function sayIt()
+function vending_machine($price, $juice_name)
 {
-    $ws = <<< "StufftoSay"
-    The quick brown fox
-    Jumps over the lazy dog.
-    StufftoSay;
-    return $ws;
+    if ($price >= 120) {
+        return $juice_name . "のお買い上げありがとうございます!<br>";
+    } else {
+        return $juice_name . "の購入金額が不足しています。<br>";
+    }
 }
 
-echo sayIt();
+echo vending_machine(120, "オレンジジュース");
+
+$price = 90;
+$juice_name = "アップルジュース";
+echo vending_machine($price, $juice_name);
